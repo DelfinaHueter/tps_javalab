8954256e5a36846e74aae6efa75563ff731c5528
Del <delfinahueter@gmail.com>
2024-10-20 23:56:01 -0300

    Update ej1.java

--- funciones/ej1.java
+++ funciones/ej1.java
@@ -1,1 +1,36 @@
 
+import java.util.Scanner;
+/*
+Una pequeña tienda registra sus ventas diarias en un arreglo. Se necesita escribir una
+función que use un bucle para calcular los ingresos totales mensuales.
+**Instrucciones**:
+- Crea un arreglo `ventas` que almacene las ventas diarias de una tienda durante 30 días.
+- Escribe una función `calcularIngresosMensuales` que recorra el arreglo y sume las ventas.
+- Imprime el total de ingresos mensuales.
+
+ */
+public class ej1 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        float ventas[] = new float[30];
+
+        System.out.println("Ventas totales del mes: "+ ventasMensuales(ventas));
+    }
+
+    public static float ventasMensuales(float[] ventas) {
+
+        float suma = 0;
+
+        for (int i = 0; i < ventas.length; i++) {
+
+            System.out.println("Ingrese las ventas del dia "+(i+1));
+            ventas[i] = sc.nextFloat();
+
+            suma += ventas[i];
+        }
+        return suma;
+    }
+}
