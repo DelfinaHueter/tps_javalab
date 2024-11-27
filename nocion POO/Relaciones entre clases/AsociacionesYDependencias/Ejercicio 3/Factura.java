5bd34cbed183d2352086e76d39272814dc30ae61
Del <delfinahueter@gmail.com>
2024-11-27 14:56:12 -0300

    Create Factura.java

---
+++ nocion POO/Relaciones entre clases/AsociacionesYDependencias/Ejercicio 3/Factura.java
@@ -0,0 +1,59 @@
+package AsociacionYDependencia;
+
+import java.util.ArrayList;
+
+public class Factura {
+    private String fechaFactura;
+    private long numeroFactura;
+    private double totalCalculadoFactura;
+    private String cliente;
+    private ArrayList<DetalleFactura> detalles = new ArrayList<>();
+
+    public String getFechaFactura() {
+        return fechaFactura;
+    }
+
+    public void setFechaFactura(String fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    public long getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(long numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public double getTotalCalculadoFactura() {
+        return totalCalculadoFactura;
+    }
+
+    public void setTotalCalculadoFactura(double totalCalculadoFactura) {
+        this.totalCalculadoFactura = totalCalculadoFactura;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<DetalleFactura> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(ArrayList<DetalleFactura> detalles) {
+        this.detalles = detalles;
+    }
+
+    public void calcularMontoTotal() {
+        totalCalculadoFactura = 0;
+        for (DetalleFactura detalle : detalles) {
+            totalCalculadoFactura += detalle.getSubTotal();
+        }
+    }
+}
+
