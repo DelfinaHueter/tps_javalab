9c3ef267d9cde814b76b1ab4d1f0a0fc26187005
Del <delfinahueter@gmail.com>
2024-11-27 14:56:54 -0300

    Create Articulos.java

---
+++ nocion POO/Relaciones entre clases/AsociacionesYDependencias/Ejercicio 3/Articulos.java
@@ -0,0 +1,39 @@
+package AsociacionYDependencia;
+
+public class Articulos {
+
+    private long codigo;
+    private String descripcion;
+    private double precio;
+
+    public Articulos(long codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
+
